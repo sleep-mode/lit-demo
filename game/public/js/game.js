ac4bb80dc0745aef004c01090da8ee32e58147bf
mutal09 <taejja09@gmail.com>
2021-05-22 03:50:27 +0900

    implemented sample game with coins and enemies

--- game/public/js/game.js
+++ game/public/js/game.js
@@ -1,21 +1,50 @@
+let width = 600;
+let speed = 50;
+let coinSpawn = 1;
+let lastCoinSpawn = 1;
+let basicEnemySpawn = 3;
+let enemySpawn = 3;
+let lastEnemySpawn = 0;
+let enemyLife = 5;
+let score = 0;
+
 function Game () { 
   var self = this;
   self.players = {};
+  self.coins = [];
+  self.enemies = [];
+  self.pid = -1;
+  self.name = "";
   addEventListener('keydown', self.handleKeyboard.bind(self));
 };
 
-Game.prototype.initPlayer = function() {
+Game.prototype.initPlayer = function(name) {
   return {
+    name: name,
     x: 10,
     y: 50,
-    direction: 1
+    direction: -1
+  }
+}
+
+Game.prototype.spawnCoin = function() {
+  return {
+    x: Math.random() * width,
+    y: 50
+  }
+}
+
+Game.prototype.spawnEnemy = function() {
+  return {
+    life: enemyLife,
+    x: Math.random() * width,
+    y: 50
   }
 }
 
 Game.prototype.handleKeyboard = function(event) {
   var self = this;
-  console.log('gotcha');
-  if ((event.keyCode || event.which) == 16) { 
+  if ((event.keyCode || event.which) == 32) { 
     if (self.players[clientId]) {
       self.players[clientId].direction *= -1;
       emit('playerDirection', {
@@ -67,12 +96,28 @@ Game.prototype.handleNetwork = function(socket) {
 }
 
 Game.prototype.updatePlayer = function(delta, player) {
-  player.x += player.direction * 30 * delta;  
-  if (player.direction > 0 && player.x > 300) {
+  player.x += player.direction * speed * delta;  
+  if (player.direction > 0 && player.x > width) {
     player.x = 0;
   }
   if (player.direction < 0 && player.x < 0) {
-    player.x = 300;
+    player.x = width;
+  }
+}
+
+Game.prototype.checkCollision = function(player, objects, callback) {
+  const threshold = 1;
+  const removeTarget = [];
+  for(let i = 0; i < objects.length; i++) {
+    if (Math.abs(player.x - objects[i].x) < threshold) {
+      removeTarget.push(objects[i]);
+    }
+  }
+
+  for(let i = 0; i < removeTarget.length; i++) {
+    if (objects.splice(objects.indexOf(removeTarget[i]), 1).length > 0) {
+      callback();
+    }
   }
 }
 
@@ -82,6 +127,30 @@ Game.prototype.handleLogic = function(delta) {
   for (var i = 0; i < Object.keys(self.players).length; i++) {
     var pid = Object.keys(self.players)[i];
     self.updatePlayer(delta, self.players[pid]);
+    self.checkCollision(self.players[pid], self.coins, () => {score++;});
+    self.checkCollision(self.players[pid], self.enemies, () => {score = 0;});
+  }
+
+  lastCoinSpawn += delta;
+  if (lastCoinSpawn > coinSpawn) {
+    lastCoinSpawn = 0;
+    self.coins.push(self.spawnCoin());
+  }
+
+  enemySpawn = Math.max(0.5, basicEnemySpawn - score * 0.3);
+
+  lastEnemySpawn += delta;
+  if (lastEnemySpawn > enemySpawn) {
+    lastEnemySpawn = 0;
+    self.enemies.push(self.spawnEnemy());
+  }
+
+  for (let i = self.enemies.length - 1; i >= 0; i--) {
+    let enemy = self.enemies[i];
+    enemy.life -= delta;
+    if (enemy.life < 0) {
+      self.enemies.splice(i, 1);
+    }
   }
 }
 
@@ -93,12 +162,11 @@ Game.prototype.handleGraphics = function(graphics) {
   graphics.fillRect(0, 0, screenWidth, screenHeight);
 
   graphics.fillStyle = '#222';
-  graphics.fillRect(0, 40, 300, 20);
+  graphics.fillRect(0, 40, width, 20);
 
   for (var i = 0; i < Object.keys(self.players).length; i++) {
     graphics.beginPath();
     var pid = Object.keys(self.players)[i];
-    console.log('Drawing ', pid);
     if (pid == clientId) {
       graphics.fillStyle = '#fff';
     } else {
@@ -106,10 +174,26 @@ Game.prototype.handleGraphics = function(graphics) {
     }
     graphics.arc(parseInt(self.players[pid].x), parseInt(self.players[pid].y), 3, 0, Math.PI * 2, true);
     graphics.fill();
+    graphics.fillText(pid, self.players[pid].x, parseInt(self.players[pid].y + 20));
+  }
+
+  for (let i = 0; i < self.coins.length; i++) {
+    graphics.beginPath();
+    graphics.fillStyle = '#ff0';
+    graphics.arc(parseInt(self.coins[i].x), parseInt(self.coins[i].y), 3, 0, Math.PI * 2, true);
+    graphics.fill();
+  }
+
+  for (let i = 0; i < self.enemies.length; i++) {
+    graphics.beginPath();
+    graphics.fillStyle = '#f00';
+    graphics.arc(parseInt(self.enemies[i].x), parseInt(self.enemies[i].y), 3, 0, Math.PI * 2, true);
+    graphics.fill();
   }
   
   graphics.fillStyle = '#FFF';
   graphics.font = '12px Tahoma';
   var playerCount = Object.keys(self.players).length;
   graphics.fillText('Total players: ' + playerCount, 10, 10);
+  graphics.fillText('Score: ' + score, 10, 20);
 }
